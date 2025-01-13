9c7e9932010aa0bad9c067ea7e19f1bfbfddf978
Riccardo <panettariccardo97@gmail.com>
2025-01-13 18:09:34 +0100

    Pippo roteante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PaperopoliComponent} from './paperopoli/paperopoli.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaperopoliComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
