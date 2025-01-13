774c6d433dcfb9c882a898074402193abe681308
Riccardo <panettariccardo97@gmail.com>
2025-01-13 18:06:37 +0100

    Pippo roteante

--- src/app/paperopoli/paperopoli.component.ts
+++ src/app/paperopoli/paperopoli.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-paperopoli',
   imports: [],
   templateUrl: './paperopoli.component.html',
+  standalone: true,
   styleUrl: './paperopoli.component.css'
 })
 export class PaperopoliComponent {
